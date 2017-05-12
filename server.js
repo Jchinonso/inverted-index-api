81d0605ed1a3da4ea3acc4d294595386f513e026
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-05-12 11:05:25 +0100

    included server file

---
+++ server.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from './route/route';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.listen(process.env.PORT, () => {
+  // console.log('server now running at ' + process.env.PORT);
+});
+
+// load the routes
+app.use('/', router);
+
+export default app;
