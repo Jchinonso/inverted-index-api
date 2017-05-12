344305c0e3a33685f04c8c9eb27dc712eb2fabd0
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-05-12 11:04:20 +0100

    refactored search route

---
+++ route/route.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import multer from 'multer';
+import invertedIndex from '../src/inverted-index';
+import endpointsValidation from './endpoints-validation';
+
+// specify the uploading directory
+const upload = multer({ dest: 'fixtures/' }).array('fileContent');
+// This is used for search endpoint since no uploading is involved
+const search = multer();
+const router = express.Router();
+
+
+// create endpoint
+router.post('/api/create', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+    res.json(endpointsValidation.create(req.body.fileName, req.files));
+  });
+});
+
+
+// search endpoint
+router.post('/api/search', search.single(), (req, res) => {
+  res.json(endpointsValidation.search(invertedIndex.bookIndex,
+   req.body.fileName, req.body.searchTerms));
+});
+
+export default router;
